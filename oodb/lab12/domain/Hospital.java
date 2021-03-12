73b316ab787ea24888625c0a47625c0ae5a88844
VikaYakunina <62566934+VikaYakunina@users.noreply.github.com>
2021-03-12 23:48:17 +0300

    Create Hospital.java

---
+++ oodb/lab12/domain/Hospital.java
@@ -0,0 +1,49 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+
+@Entity
+public class Hospital {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String hospitalName;
+
+    @Column
+    private String hospitalAddress;
+
+    public Hospital() {
+    }
+
+    public Hospital(String hospitalName, String hospitalAddress) {
+        this.hospitalName = hospitalName;
+        this.hospitalAddress = hospitalAddress;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getHospitalAddress() {
+        return hospitalAddress;
+    }
+
+    public void setHospitalAddress(String hospitalAddress) {
+        this.hospitalAddress = hospitalAddress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
