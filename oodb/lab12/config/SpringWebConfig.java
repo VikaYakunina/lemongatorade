abb33331cce0ce2cc115a6e63742507e550329d4
VikaYakunina <62566934+VikaYakunina@users.noreply.github.com>
2021-03-12 23:46:27 +0300

    Create SpringWebConfig.java

---
+++ oodb/lab12/config/SpringWebConfig.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"Hospital"})
+public class SpringWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+               .addResourceLocations("classpath:/resources/");
+    }
+}
